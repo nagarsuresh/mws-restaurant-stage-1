39fb85fc24ce37c93ae7da4e9304b512c221b4a3
Suresh Nagar <nagars@vmware.com>
2018-10-28 14:59:48 -0700

    adding alt text and aria roles

--- js/main.js
+++ js/main.js
@@ -158,7 +158,6 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
-  li.tabIndex = 0;
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
